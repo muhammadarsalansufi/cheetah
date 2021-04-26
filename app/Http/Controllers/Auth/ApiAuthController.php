b47e3e93dea79676d73910fb5b6c5d0357eb6ee9
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-04-26 17:28:25 +0500

    keys ignore logout

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -73,10 +73,10 @@ class ApiAuthController extends Controller
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     public function logout (Request $request) {
-        $token = $request->user()->token();
-        $token->revoke();
-        $response = ['message' => 'You have been successfully logged out!'];
-        return response($response, 200);
+        auth()->user()->token()->revoke();
+        return response()->json([
+            'message'=> 'Successfully logged out'
+        ]);
     }
 
 
