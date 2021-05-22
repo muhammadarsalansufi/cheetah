ea712f06ad611d8654952d1e158dc61c4bc42fc5
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-05-22 17:17:47 +0500

    login return extra

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -44,28 +44,30 @@ class ApiAuthController extends Controller
      * @return \Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
      */
     public function login (Request $request) {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email|max:255',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-        if ($validator->fails())
-        {
-            return response(['errors'=>$validator->errors()->all()], 422);
-        }
-        $user = User::where('email', $request->email)->first();
-        if ($user) {
-            if (Hash::check($request->password, $user->password)) {
-                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
-                $response = ['message'=>'True','token' => $token];
-                return response($response, 200);
-            } else {
-                $response = ["message" => "Password mismatch"];
-                return response($response, 422);
-            }
-        } else {
-            $response = ["message" =>'User does not exist'];
-            return response($response, 422);
-        }
+       $response = ["message" =>'User does not exist'];
+        return response($response, 200);
+//        $validator = Validator::make($request->all(), [
+//            'email' => 'required|string|email|max:255',
+//            'password' => 'required|string|min:6|confirmed',
+//        ]);
+//        if ($validator->fails())
+//        {
+//            return response(['errors'=>$validator->errors()->all()], 422);
+//        }
+//        $user = User::where('email', $request->email)->first();
+//        if ($user) {
+//            if (Hash::check($request->password, $user->password)) {
+//                $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+//                $response = ['message'=>'True','token' => $token];
+//                return response($response, 200);
+//            } else {
+//                $response = ["message" => "Password mismatch"];
+//                return response($response, 422);
+//            }
+//        } else {
+//            $response = ["message" =>'User does not exist'];
+//            return response($response, 422);
+//        }
     }
 
     /**
