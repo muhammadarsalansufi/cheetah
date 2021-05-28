6b9db17e2b75c6060cc2b9093d43b1233e7a9ed3
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-05-28 16:56:52 +0500

    auth type code

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,7 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/articles', 'ArticleController@index')->middleware('api.admin')->name('articles');
-    Route::post('/test', 'SuperAdmin\SuperController@test')->middleware('api.superAdmin')->name('test');
+    Route::post('/test', 'SuperAdmin\SuperController@test')->middleware('api.admin')->name('test');
 
 });
 
