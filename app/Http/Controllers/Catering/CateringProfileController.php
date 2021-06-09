1132e0102331e99e0035d6e665a44691d6457e0a
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-06-09 21:29:19 +0500

    edit catering

--- app/Http/Controllers/Catering/CateringProfileController.php
+++ app/Http/Controllers/Catering/CateringProfileController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\Catering;
 
 
+use App\CateringServiceProvider;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,32 @@ class CateringProfileController extends Controller
 {
     public function addprofile(Request $request)
     {
+        $id = $request->user_id;
+        $editcatering = CateringServiceProvider::where('user_id','=',$id)->update([
+            'company_name' => $request->company_name,
+            'company_email' => $request->company_email,
+            'status' => 'inActive',
+            'zipcode' => $request->zipcode,
+            'landline' => $request->landline,
+            'mobile' => $request->mobile,
+            'fax' => $request->fax,
+            'address' => $request->address,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            'city' => $request->city,
+            'state' => $request->state
+        ]);
+        if($editcatering == 1)
+        {
+            $message = ["status" => "True"];
+
+        }
+        else
+        {
+            $message = ["status" => "False"];
+
+        }
+        return response($message, 200);
 
 
     }
