9eda98dc031a1e091d5ea92ace6d3cbbacca1a58
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-06-12 19:01:02 +0500

    resturant

---
+++ app/CateringOrder.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class CateringOrder extends Model
+{
+   protected $table="catering_order";
+}
--- app/Http/Controllers/Catering/GlobalCateringController.php
+++ app/Http/Controllers/Catering/GlobalCateringController.php
@@ -4,9 +4,25 @@
 namespace App\Http\Controllers\Catering;
 
 
+use App\CateringOrder;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Client\Request;
 
 class GlobalCateringController extends Controller
 {
+    public function book_order(Request $request)
+    {
+        $order =  new CateringOrder();
+        $order->username = $request->username;
+        $order->catering_id = $request->catering_id;
+        $order->date = $request->date;
+        $order->email = $request->email;
+        $order->phone = $request->phone;
+        $order->save();
+        $message = ["status" => "True"];
+        return response($message, 200);
+
+
+    }
 
 }
