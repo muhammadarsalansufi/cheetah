a9474bc2ff375acac3bfda4a9d731d4abcfb21d8
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-06-14 17:40:36 +0500

    staff tessst

--- app/Http/Controllers/Catering/CateringProfileController.php
+++ app/Http/Controllers/Catering/CateringProfileController.php
@@ -17,7 +17,7 @@ class CateringProfileController extends Controller
 {
     public function addprofile(Request $request)
     {
-        $message = ["status" => $request->catering_staff[0]];
+        $message = ["status" => $request->all()];
         return response($message, 200);
         $id = auth()->user()->id;
         $logoImg = 'not defined';
