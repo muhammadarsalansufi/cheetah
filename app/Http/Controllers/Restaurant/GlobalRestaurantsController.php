713f732c6754830ed20e5ff3082da3a1f8a41900
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-06-16 20:09:13 +0500

    featured_image

--- app/Http/Controllers/Restaurant/GlobalRestaurantsController.php
+++ app/Http/Controllers/Restaurant/GlobalRestaurantsController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Restaurant;
 use App\CateringContent;
 use App\CateringStaff;
 use App\Http\Controllers\Controller;
+use App\RestaurantFeatured;
 use App\RestuarantContent;
 use App\Resturants;
 use App\Testimonials;
@@ -25,9 +26,11 @@ class GlobalRestaurantsController extends Controller
         $user_id =  RestuarantContent::where('id','=',$id)->where('status','=','Active')->where('admin_status','=','Approved')->pluck('user_id')->first();;
         $cateringservices = RestuarantContent::where('id','=',$user_id)->where('status','=','Active')->where('admin_status','=','Approved')->get();
         $cateringtesti = Resturants::select()->where('user_id','=',$user_id)->get();
+        $featured_image = RestaurantFeatured::select()->where('user_id','=',$user_id)->where('status','=','Active')->get();
          $message = ["status" => "True",
             "Content" => $cateringservices,
-            "Profile" => $cateringtesti];
+            "Profile" => $cateringtesti,
+            "Featured" => $featured_image];
         return response($message, 200);
     }
 
