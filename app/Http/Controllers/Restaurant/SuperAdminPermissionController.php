64918e88195a72c38fb8b0c4dc27f6b8e660370f
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-06-24 16:37:45 +0500

    super admin

--- app/Http/Controllers/Restaurant/SuperAdminPermissionController.php
+++ app/Http/Controllers/Restaurant/SuperAdminPermissionController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Restaurant;
 
 use App\Http\Controllers\Controller;
 use App\Resturants;
+use Illuminate\Http\Request;
 
 class SuperAdminPermissionController extends Controller
 {
@@ -15,6 +16,13 @@ class SuperAdminPermissionController extends Controller
         $message = ["status" => "True",
             "Content" => $record];
         return response($message, 200);
+    }
+    public function updaterestaurantlistitem(Request $request)
+    {
+        $id = $request->id;
+        $userid = Resturants::where('id','=',$id)->pluck('user_id')->first();
+        $updateprofile = Resturants::where('user_id','=',$userid)->get();
+
     }
 
 }
