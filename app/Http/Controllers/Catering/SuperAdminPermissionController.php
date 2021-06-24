f7111f416e5fc2d78cb5d7db3c843e971cd33557
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-06-24 19:35:10 +0500

    approval added

--- app/Http/Controllers/Catering/SuperAdminPermissionController.php
+++ app/Http/Controllers/Catering/SuperAdminPermissionController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\Catering;
 
 
+use App\CateringContent;
 use App\CateringServiceProvider;
 use App\Http\Controllers\Controller;
 use App\RestuarantContent;
@@ -20,37 +21,37 @@ class SuperAdminPermissionController extends Controller
             "Content" => $record];
         return response($message, 200);
     }
-    public function updaterestaurantlistitem(Request $request)
+    public function updatecateringlistitem(Request $request)
     {
         $id = $request->id;
-        $userid = Resturants::where('id','=',$id)->pluck('user_id')->first();
-        $updateprofile = Resturants::where('user_id','=',$userid)->update([
+        $userid = CateringServiceProvider::where('id','=',$id)->pluck('user_id')->first();
+        $updateprofile = CateringServiceProvider::where('user_id','=',$userid)->update([
             'status'=>$request->status,
-            'admin_status'=>$request->admin_status,
-            'restaurant_name'=>$request->restaurant_name,
-            'restaurant_email'=>$request->restaurant_email,
-            'landline'=>$request->landline,
+            'approval_status'=>$request->approval_status,
+            'email_status'=>$request->email_status,
+            'company_name'=>$request->company_name,
+            'company_email'=>$request->company_email,
             'mobile'=>$request->mobile,
             'address'=>$request->address,
             'zipcode'=>$request->zipcode,
             'city'=>$request->city
 
         ]);
-        $updatecontent  = RestuarantContent::where('user_id','=',$userid)->update([
+        $updatecontent  = CateringContent::where('user_id','=',$userid)->update([
             'status'=>$request->status,
-            'restaurant_name'=>$request->restaurant_name,
-            'admin_status'=>$request->admin_status,
+            'company_title'=>$request->company_name,
+            'admin_status'=>$request->approval_status,
         ]);
         $message = ["status" => "True"];
         return response($message, 200);
 
     }
-    public function deleterestaurantlistitem(Request $request)
+    public function deletecateringlistitem(Request $request)
     {
         $id = $request->id;
-        $userid = Resturants::where('id','=',$id)->pluck('user_id')->first();
-        $updateprofile = Resturants::where('user_id','=',$userid)->delete();
-        $updatecontent  = RestuarantContent::where('user_id','=',$userid)->delete();
+        $userid = CateringServiceProvider::where('id','=',$id)->pluck('user_id')->first();
+        $updateprofile = CateringServiceProvider::where('user_id','=',$userid)->delete();
+        $updatecontent  = CateringContent::where('user_id','=',$userid)->delete();
         $user  = User::where('id','=',$userid)->delete();
         $message = ["status" => "True"];
         return response($message, 200);
