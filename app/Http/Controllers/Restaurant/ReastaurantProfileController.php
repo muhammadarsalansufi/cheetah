f48bb4ba1c98e69e4b8c74a86e804315fbce31e6
muhammadarsalansufi <muhammadarsalanasufi@gmail.com>
2021-10-09 15:10:47 +0500

    new

--- app/Http/Controllers/Restaurant/ReastaurantProfileController.php
+++ app/Http/Controllers/Restaurant/ReastaurantProfileController.php
@@ -262,7 +262,7 @@ class ReastaurantProfileController extends Controller
 //       $id =  auth()->user()->id;
        $orders = FoodOrder::where('delivery_status','=','pending')->pluck('food_array')->first();
        $data = json_decode($orders);
-        $message = ["status" => "True", "record" => $data];
+        $message = ["status" => "True", "record" => $orders];
         return response($message, 200);
 //       foreach($data as $dataorder)
 //       {
